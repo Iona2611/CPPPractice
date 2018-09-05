02b054c4323653c387c6dc424f3709676fb78094
Iona2611 <43002101+Iona2611@users.noreply.github.com>
2018-09-05 14:44:33 +0100

    Added Hello World message

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,14 @@
 // Include Libraries 
 #include <stdlib.h> // Standard library
-
+#include  <iostream> // input/output library 
 
 // Entry point for our program
 // Every C++ program needs a main() function
 int main()
 {
+	std::cout << "Hello World! " << std::endl;
+	
+	
 	// wait for user to press button before continuing
 	system("PAUSE");
 
